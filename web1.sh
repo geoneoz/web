9a82026e5c38c6f8bf83e0feaf5794905b7867d0
geno <alvinfairani@gmail.com>
2026-01-12 14:41:19 +0700

    Update web1.sh

--- web1.sh
+++ web1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. Update the package list first so apt can find the packages
-apt update
+apt update && apt upgrade 
 
 # NGINX
 echo "Installing nginx......."
@@ -16,19 +16,18 @@ echo "Installing finish"
 # MARIADB
 echo "Installing mariadb........"
 # Added mariadb-server to ensure the database actually runs
-apt install -y mariadb-client mariadb-server
-echo "Installing mariadb-client finish"
+apt install -y mariadb-client
+echo "Installing mariadb finish"
 
 # PHP REQUIREMENT
 # Fixed the typo: removed the extra dot after 8.2 (php8.2-)
 # Removed 'json' because it is built into PHP 8.2 and 'xmlrpc' which is no longer in main repos
-apt install -y php8.2-{fpm,bcmath,bz2,cli,common,curl,dev,gd,gmp,imagick,imap,intl,mbstring,mysql,opcache,soap,ssh2,xml,zip}
-
-# Fixed the path: PHP config is in /etc/, not /var/
+echo "Installing php req"
+apt install -y php8.2-xml php8.2-intl php8.2-mbstring php8.2-xmlrpc php8.2-soap php8.2-bcmath php8.2-exif php8.2-ldap php8.2-mysql unzip php8.2-fpm php8.2-cli php8.2-curl php8.2-zip php8.2-gd
 rm -f /etc/php/8.2/fpm/php.ini
 mv web/php.ini /etc/php/8.2/fpm/
-# Added a restart to apply the new php.ini
 systemctl restart php8.2-fpm
+echo "Installing php finish"
 
 # MOODLE
 echo "Installing moodle...."
