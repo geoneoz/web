b798818446d10168f08d464476b531962763ea47
geno <alvinfairani@gmail.com>
2026-01-15 07:58:56 +0700

    Update config.php

--- config.php
+++ config.php
@@ -161,8 +161,14 @@ $CFG->dboptions = array(
 //
 // If you need both intranet and Internet access please read
 // http://docs.moodle.org/en/masquerading
-
-$CFG->wwwroot   = 'http://'.$_SERVER['HTTP_HOST'];
+$alamat_akses = $_SERVER['HTTP_HOST'];
+if (strpos($alamat_akses, '172.16.100.36') !== false) {
+    $CFG->wwwroot = 'http://172.16.100.36:9090';
+    $_SERVER['SERVER_PORT'] = 9090;
+} else {
+    $CFG->wwwroot = 'http://192.168.36.2';
+}
+// $CFG->wwwroot   = 'http://'.$_SERVER['HTTP_HOST'];
 
 
 //=========================================================================
